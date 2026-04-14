2db79f0d46d71e91deebe47a295de6bcd5c3690d
Sesilyo <c.sethleander@gmail.com>
2026-04-14 10:30:34 +0800

    what if... block?

--- scripts/announcementSlider.js
+++ scripts/announcementSlider.js
@@ -1,18 +1,12 @@
 /* announcementSlider.js */
 
 const slider = document.getElementById("announcement-slider");
-const cards  = document.querySelectorAll(".announcement-card");
+const rows  = document.querySelectorAll(".announcement-card");
 
 slider.addEventListener("input", () => {
     const visible = parseInt(slider.value);
 
-    cards.forEach((card, index) => {
-        if (index < visible) {
-            card.style.display = "flex";
-        }
-
-        else {
-            card.style.display = "none";
-        }
-    })
-})
+    rows.forEach((row, index) => {
+        row.style.display = index < visible ? "grid" : "none";
+    });
+});
